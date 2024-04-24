ceeb679102c4d30b62d35dfc356174557e7f85f6
patryk115 <patryk.kindra2ia@gmail.com>
2024-04-24 19:10:16 +0200

    Stworzenie wyglądu TranslatorActivity

---
+++ app/src/main/java/com/example/langmaster/TranslatorActivity.java
@@ -0,0 +1,13 @@
+package com.example.langmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TranslatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_translator);
+    }
+}
